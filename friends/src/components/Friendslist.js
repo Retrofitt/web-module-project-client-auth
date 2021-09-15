f497cd1610bf02ca6ec1273b90caa83c11cdb43c
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-14 21:33:35 -0700

    added friend component

--- friends/src/components/Friendslist.js
+++ friends/src/components/Friendslist.js
@@ -1,13 +1,37 @@
+import React from "react";
+import { Route } from "react-router";
+import axiosWithAuth from "../utils/axiosWithAuth";
+import Friend from "./Friend";
+class FriendsList extends React.Component{
+    state={
+        friends:[],
+    }
 
-export default function FriendsList(){
+    componentDidMount(){
+        axiosWithAuth()
+            .get('/friends')
+            .then(res=>{
+                this.setState({
+                    ...this.state.friends,
+                    friends: res.data
+                })
+            })
+            .catch(err=>{
+                console.error(err)
+            })
+    }   
+
+
+    render(){
+        return(<div>
+            {this.state.friends.map((friend, id)=>{
+                return(<Friend key={id} friend={friend}/>)
+            })}
+        </div>)
+    }
+} 
+
+export default FriendsList
 
 
 
-
-
-
-    
-    return(<div className='friends-list-component'>
-        <h2>TestElement</h2>
-    </div>)
-}
