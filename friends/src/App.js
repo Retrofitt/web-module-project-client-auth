eb779d175883b3cae5f172e031f30a6e6d59d223
Retrofitt <rafaelmendozajr94@gmail.com>
2021-09-14 20:43:39 -0700

    can log in and out

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+import axios from 'axios'
 import './App.css';
 
+import {Switch, Link, Route} from 'react-router-dom'
+
+import Login from './components/Login'
+import FriendsList from './components/Friendslist';
+import Logout from './components/Logout';
+import ProtectedRoute from './components/ProtectedRoute';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return (<div className="App">
+
+    <Link to='/login'>Login</Link>
+    <Link to='/logout'>Logout</Link>
+    
+    
+
+    <Switch>
+      <ProtectedRoute exact path='/protected' component={FriendsList}/>
+      <Route path='/logout' component={Logout} />
+      <Route path='/login' component={Login} />
+      <Route path='/' component={Login}/>
+    </Switch>
+    
+  </div>);
 }
 
 export default App;
---
+++ friends/src/components/Friendslist.js
@@ -0,0 +1,13 @@
+
+export default function FriendsList(){
+
+
+
+
+
+
+    
+    return(<div className='friends-list-component'>
+        <h2>TestElement</h2>
+    </div>)
+}
